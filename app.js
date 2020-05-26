961031d197f057bd1e6664e40e583218ae8652e9
张晓东 <449422301@qq.com>
2020-05-26 15:41:37 +0800

    提交

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-const express = require('express')const  app = express()app.get('/',(req,res)=>{  res.send('hello word')})app.listen(8080)console.log('服务器已启动')
+const express = require('express')const  app = express()app.get('/',(req,res)=>{  res.send('hello word')})app.listen(3000)console.log('服务器已启动')
